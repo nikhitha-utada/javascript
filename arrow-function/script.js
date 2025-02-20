1cdce9c289fb01f9c354d3f61763bc08f3f158eb
nikhitha-utada <nikhithautada2003@gmail.com>
2025-02-21 00:40:20 +0530

    arrow functions taks

---
+++ arrow-function/script.js
@@ -0,0 +1,46 @@
+// que-1:
+/**
+ * arrow function is a function which doesn't have a function name and function keyword
+ * syntax:
+ * var refvar = () => {
+ *                      //statements
+ *                      //return keyword
+ *                      }
+ */
+
+// que-2:
+var add = (a,b)=>{
+    return a+b;
+}
+console.log(add(2,3));
+
+// que-3:
+var square = n => n*n;
+console.log(square(7));
+
+// que-4:
+// var fn = parameter => {statemets}
+// when there is only one parameter we can omit the paranthesis
+
+// que-5:
+// v-1:
+var cube = n => n*n*n;
+console.log(cube(6));
+
+// v-2:
+var cube = n => {
+    return n*n*n;
+}
+console.log(cube(9));
+
+// que-6:
+var diff = (a,b)=>{
+    return b-a;
+}
+console.log(diff(9,8));
+// if there are more than 1 parameter in the function it is mandatory to put () around the parameters...
+
+
+// que-7:
+var msg = () => "Hello,world!";
+console.log(msg());
