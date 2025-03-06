84017fd5b4e6c86b7b6bdd9ae81bc64f2d0a8031
nikhitha-utada <nikhithautada2003@gmail.com>
2025-03-06 18:14:00 +0530

    sum from 1 to 100

--- for-loops/script.js
+++ for-loops/script.js
@@ -5,3 +5,11 @@ for(var i=1;i<=10;i++){
     console.log(i);
 }
 
+
+// Scenario 2: Sum of Numbers
+// Write a for loop to calculate the sum of all numbers from 1 to 100.
+var sum = 0;
+for(var i=1;i<=100;i++){
+    sum+=i;
+}
+console.log(`sum of 1 to 100 numbers is ${sum}`);
