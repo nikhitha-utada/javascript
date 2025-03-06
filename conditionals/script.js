4003fa1633573e61d4e4a32efbacfa0a4f25c941
nikhitha-utada <nikhithautada2003@gmail.com>
2025-03-06 18:39:29 +0530

    - A bus company offers a discount for students under 18 and senior citizens above 60. Write a condition to determine if a person gets a discount based on their age.

---
+++ conditionals/script.js
@@ -0,0 +1,9 @@
+//A bus company offers a discount for students under 18 and senior citizens above 60. Write a condition to determine if a person gets a discount based on their age.  
+
+var age = 45
+if(age<18 || age>60){
+    console.log("Congratulations you got discount");
+}
+else{
+    console.log("Sorry you have no discount");
+}
